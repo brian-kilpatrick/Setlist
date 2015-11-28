183f05753820ff3fbbb75fa65fc8b11f7114940e
Brian Kilpatrick <brianokilpatrick@gmail.com>
2015-11-28 15:09:23 -0500

    Added Home Page styling

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -1,20 +1,21 @@
 class SetlistsController < ApplicationController
 	before_action :authenticate_user!
+	before_action :set_setlist, except: [:create, :update]
 	def new
 		
 	end
 
 	def create
-		@setlist = Setlist.create
+		@setlist = Setlist.create(organization_id: @current_org.id, name: "New Setlist #{Time.now.to_s(:long)}")
 		redirect_to edit_setlist_path(@setlist)
 	end
 
 	def show
-		@setlist = Setlist.find(params[:id])
+	
 	end
 
 	def edit
-		@setlist = Setlist.find(params[:id])
+	
 	end
 
 	def sort
@@ -22,8 +23,8 @@ class SetlistsController < ApplicationController
 	end
 
 	def update
-		# @setlist = Setlist.find(params[:id])
-		# @setlist.update_attributes(setlist_params)
+		# need to get @setlist from prior params id
+		@setlist.update_attributes(setlist_params)
 		if !params[:order].nil?
 			params[:order].each do |key,value|
 				Item.find(value[:id]).update_attribute(:position,value[:position])
@@ -35,13 +36,17 @@ class SetlistsController < ApplicationController
 	end
 
 	def destroy
-		@setlist = Setlist.find(params[:id])
+	
 		@setlist.destroy
 		flash[:message] = "Setlist Deleted!"
 		redirect_to root_path
 	end
 
 	private
+
+	def set_setlist
+		@setlist = Setlist.find(params[:id])
+	end
 
 	def setlist_params
 		params.require(:setlist).permit(:name,:performance_date, :performance_time, :rehearsal_date, :rehearsal_time)
